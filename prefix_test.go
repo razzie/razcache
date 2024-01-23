3f02bff48f46473ba25e8ee483d000905d31e6c3
Gábor Görzsöny <gabor@gorzsony.com>
2024-01-23 15:00:25 +0100

    Added prefix cache tests

---
+++ prefix_test.go
@@ -0,0 +1,35 @@
+package razcache
+
+import (
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPrefixCache(t *testing.T) {
+	cache := NewInMemCache()
+	defer cache.(io.Closer).Close()
+
+	assert.NoError(t, cache.Set("a", "val_a", 0))
+	assert.NoError(t, cache.Set("prefix:b", "val_b", 0))
+
+	subcache := NewPrefixCache(cache, "prefix:")
+
+	// prefix cache should hide non-prefixed item keys
+	_, err := subcache.Get("a")
+	assert.Equal(t, ErrNotFound, err)
+
+	// previously created prefixed item key should work
+	value, err := subcache.Get("b")
+	assert.NoError(t, err)
+	assert.Equal(t, "val_b", value)
+
+	// newly created prefixed key should be visible in original cache
+	assert.NoError(t, subcache.Set("c", "val_c", 0))
+	_, err = cache.Get("c")
+	assert.Equal(t, ErrNotFound, err)
+	value, err = cache.Get("prefix:c")
+	assert.NoError(t, err)
+	assert.Equal(t, "val_c", value)
+}
