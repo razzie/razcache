f687c77718653d0aab8528c90810d45bc3ab748b
Gábor Görzsöny <gabor@gorzsony.com>
2024-05-20 00:18:41 +0200

    TTLQueue receiver tidy-up

--- internal/util/ttlqueue.go
+++ internal/util/ttlqueue.go
@@ -21,32 +21,32 @@ func (i TTLItem[T]) Expiration() time.Time {
 
 type ttlQueueImpl[T any] []*TTLItem[T]
 
-func (eq ttlQueueImpl[T]) Len() int { return len(eq) }
+func (ttlq ttlQueueImpl[T]) Len() int { return len(ttlq) }
 
-func (eq ttlQueueImpl[T]) Less(i, j int) bool {
-	return eq[i].expiration.Before(eq[j].expiration)
+func (ttlq ttlQueueImpl[T]) Less(i, j int) bool {
+	return ttlq[i].expiration.Before(ttlq[j].expiration)
 }
 
-func (eq ttlQueueImpl[T]) Swap(i, j int) {
-	eq[i], eq[j] = eq[j], eq[i]
-	eq[i].index = i
-	eq[j].index = j
+func (ttlq ttlQueueImpl[T]) Swap(i, j int) {
+	ttlq[i], ttlq[j] = ttlq[j], ttlq[i]
+	ttlq[i].index = i
+	ttlq[j].index = j
 }
 
-func (eq *ttlQueueImpl[T]) Push(x any) {
-	n := len(*eq)
+func (ttlq *ttlQueueImpl[T]) Push(x any) {
+	n := len(*ttlq)
 	item := x.(*TTLItem[T])
 	item.index = n
-	*eq = append(*eq, item)
+	*ttlq = append(*ttlq, item)
 }
 
-func (eq *ttlQueueImpl[T]) Pop() any {
-	old := *eq
+func (ttlq *ttlQueueImpl[T]) Pop() any {
+	old := *ttlq
 	n := len(old)
 	item := old[n-1]
 	old[n-1] = nil  // avoid memory leak
 	item.index = -1 // for safety
-	*eq = old[0 : n-1]
+	*ttlq = old[0 : n-1]
 	return item
 }
 
@@ -54,37 +54,37 @@ type TTLQueue[T any] struct {
 	q ttlQueueImpl[T]
 }
 
-func (eq *TTLQueue[T]) Push(value T, expiration time.Time) *TTLItem[T] {
+func (ttlq *TTLQueue[T]) Push(value T, expiration time.Time) *TTLItem[T] {
 	item := &TTLItem[T]{
 		value:      value,
 		expiration: expiration,
 	}
-	heap.Push(&eq.q, item)
+	heap.Push(&ttlq.q, item)
 	return item
 }
 
-func (eq *TTLQueue[T]) Pop() *TTLItem[T] {
-	return heap.Pop(&eq.q).(*TTLItem[T])
+func (ttlq *TTLQueue[T]) Pop() *TTLItem[T] {
+	return heap.Pop(&ttlq.q).(*TTLItem[T])
 }
 
-func (eq *TTLQueue[T]) Peek() *TTLItem[T] {
-	return eq.q[0]
+func (ttlq *TTLQueue[T]) Peek() *TTLItem[T] {
+	return ttlq.q[0]
 }
 
-func (eq *TTLQueue[T]) Update(item *TTLItem[T], value T, expiration time.Time) {
+func (ttlq *TTLQueue[T]) Update(item *TTLItem[T], value T, expiration time.Time) {
 	item.value = value
 	item.expiration = expiration
-	heap.Fix(&eq.q, item.index)
+	heap.Fix(&ttlq.q, item.index)
 }
 
-func (eq *TTLQueue[T]) Delete(item *TTLItem[T]) {
-	heap.Remove(&eq.q, item.index)
+func (ttlq *TTLQueue[T]) Delete(item *TTLItem[T]) {
+	heap.Remove(&ttlq.q, item.index)
 }
 
-func (eq *TTLQueue[T]) Len() int {
-	return eq.q.Len()
+func (ttlq *TTLQueue[T]) Len() int {
+	return ttlq.q.Len()
 }
 
-func (eq *TTLQueue[T]) Clear() {
-	eq.q = nil
+func (ttlq *TTLQueue[T]) Clear() {
+	ttlq.q = nil
 }
